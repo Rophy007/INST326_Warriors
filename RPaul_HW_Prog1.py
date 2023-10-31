ae9f355b1a1956471bc72f010e53108bd80de532
TyreseHooper <143568454+TyreseHooper@users.noreply.github.com>
2023-10-31 16:45:30 -0400

    Update RPaul_HW_Prog1.py

--- RPaul_HW_Prog1.py
+++ RPaul_HW_Prog1.py
@@ -1,7 +1,7 @@
 print ("hello world")
 print ("good morning")
 print ("good eveing")
-print ("INST 326 Warriors")
+print ("INST 326 Warriors- Tyrese Hooper")
 
 print ("yo yo-what it do - Abrar from VS")
 print( "commit sum more -Abrar from VS")
