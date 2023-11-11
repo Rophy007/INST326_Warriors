b5f75f02174439616dee33b880c811fb86ab8187
TyreseHooper <143568454+TyreseHooper@users.noreply.github.com>
2023-11-10 21:19:02 -0500

    Update Final_Group_Project.py

--- Final_Group_Project.py
+++ Final_Group_Project.py
@@ -34,3 +34,33 @@ if __name__ == "__main__":
 
     except User:
         print(f"Error") 
+
+class OnlineBankingSystem:
+    def __init__(self):
+        self.user_account= {}
+
+    def generate_account_number(self):
+        return str(random.randint(10000000, 99999999))
+
+    def create_bank_account(self, firstname, lastname, email, initial_balance=0, account_type='Savings'):
+        bank_account_number = self.generate_account_number()
+
+        user_information = {
+            "first_name": firstname,
+            "last_name": lastname,
+            "email": email,
+            "account_number": bank_account_number,
+            "balance": initial_balance,
+            "account_type": account_type
+     }
+    
+        self.user_account[bank_account_number]= user_information
+
+        print(f"\n Congratulations on making your new online banking account! Here are your account details:")
+        print(f"Account Number: {bank_account_number}")
+        print(f"Name: {firstname} {lastname}")
+        print(f"Email: {email}")
+        print(f"Initial Balance: ${initial_balance}")
+        print(f"Account Type: {account_type}")
+
+        return bank_account_number
